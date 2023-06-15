d9002bb5b6524a4f357063e3495b9f7c2347c030
R. G. Esteves <rodolfo.g.esteves@intel.com>
2023-06-15 16:13:37 -0700

    Fixing conversion of array of floats to halfs.cpp

--- testFP16.cpp
+++ testFP16.cpp
@@ -45,6 +45,38 @@ int test_fp16() {
 }
 #endif
 
+#if 0
+#include <immintrin.h>
+
+__m512h createM512h(const float* src) {
+    // Load the floats into a 512-bit wide register
+    __m512 vec_fp32 = _mm512_loadu_ps(src);
+
+    // Convert the floats to fp16
+    __m512h vec_fp16 = _mm512_castps_ph(vec_fp32);
+
+    return vec_fp16;
+}
+
+int main() {
+    // Example usage
+    float floats[16] = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f, 13.0f, 14.0f, 15.0f, 16.0f};
+
+    __m512h m512h_val = createM512h(floats);
+
+    // Access the converted fp16 values
+    uint16_t fp16_vals[32];
+    _mm512_storeu_si512((__m512i*)fp16_vals, (__m512i)m512h_val);
+
+    // Print the converted fp16 values
+    for (int i = 0; i < 16; ++i) {
+        printf("fp16_vals[%d]: %f\n", i, (float)fp16_vals[i]);
+    }
+
+    return 0;
+}
+#endif
+
 float dotProductFP16(const float* a, const float* b, int size) {
     // Make sure the size is a multiple of 16 for AVX-512
     int size_aligned = (size + 15) & ~15;
